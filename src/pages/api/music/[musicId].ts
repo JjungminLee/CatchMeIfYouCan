219fa359b5c90c42f8063ade8c19c395806b92e5
JjungminLee <julie0964@naver.com>
2023-03-24 07:16:43 +0900

    feat: 음악별 상세조회 api

--- src/pages/api/music/[musicId].ts
+++ src/pages/api/music/[musicId].ts
@@ -1,17 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
-// PrismaClient 가져오기 위한 모듈 불러오기
-const { PrismaClient } = require("@prisma/client");
-// PrismaClient 만들기
 const client = new PrismaClient();
+const POST="POST"
+const GET="GET"
 
-export default function handler(req:NextApiRequest, res:NextApiResponse) {
-    const query=req.query;
-    const {musicId} = query;
-    const music = client.user.findUnique({
-        where: {
-          id: musicId,
-        },
-      })
-    res.status(200).json({ text: {music} })
-  }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    if(req.method===GET){
+        const { musicId } = req.query;
+        const music = await client.music.findUnique({
+            where: { id: +(musicId||'0')},
+            select:{name:true,imgUrl:true}
+        });
+        return res.json({ music });
+        }
+}
